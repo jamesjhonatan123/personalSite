2b84205334166caf39ff8eb4628fba42ab526fce
jamesjhonatan <jamesjhonatan123@gmail.com>
2022-08-23 12:34:22 -0300

    restruction

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,46 +1,61 @@
 import { RiComputerLine } from 'react-icons/ri'
-import { DiResponsive } from 'react-icons/di'
+import { MdOutlinePhonelink } from 'react-icons/md'
 import { BsCodeSquare } from 'react-icons/bs'
 import { AiOutlineAntDesign } from 'react-icons/ai'
 
 import styles from './styles.module.scss'
+import { IconContext } from 'react-icons'
 
 export const Services = () => {
 
 
 
   return (
-    <div id="services" className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.card}>
-          <span>Criação de sites</span>
-          <RiComputerLine style={{ color: 'white', width: '70px', height: '70px' }} />
-          <p>
-            Desenvolvimento de sites profissionais, sistemas web, blogs e lojas virtuais.
-          </p>
+    <IconContext.Provider value={{ color: "white", size: '3rem' }}>
+
+      <div id="services" className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.card}>
+            <span>Criação de sites</span>
+            <i>
+              <RiComputerLine />
+            </i>
+            <p>
+              Desenvolvimento de sites profissionais, sistemas web, blogs e lojas virtuais.
+            </p>
+          </div>
+          <div className={styles.card}>
+            <span>Sistemas responsivos</span>
+            <i>
+
+              <MdOutlinePhonelink />
+            </i>
+            <p>
+              Desenho de layouts que se adequam ao seu negócio de forma profissional e fiel no desenvolvimento.
+            </p>
+          </div>
+          <div className={styles.card}>
+            <span>UX/UI Design</span>
+            <i>
+
+              <AiOutlineAntDesign />
+            </i>
+            <p>
+              Criação de sites responsivos para a melhor visualização em todas
+              as resoluções.
+            </p>
+          </div>
+          <div className={styles.card}>
+            <span>Código semântico</span>
+            <i>
+
+              <BsCodeSquare />
+            </i>
+            <p>
+              Estratégia e monitoração para seu negócio, fazendo com que seu site apareça cada vez melhor nas buscas.          </p>
+          </div>
         </div>
-        <div className={styles.card}>
-          <span>Sistemas responsivos</span>
-          <DiResponsive style={{ color: 'white', width: '90px', height: '90px', marginTop: '-1rem' }} />
-          <p>
-            Desenho de layouts que se adequam ao seu negócio de forma profissional e fiel no desenvolvimento.
-          </p>
-        </div>
-        <div className={styles.card}>
-          <span>UX/UI Design</span>
-          <AiOutlineAntDesign style={{ color: 'white', width: '70px', height: '70px' }} />
-          <p>
-            Criação de sites responsivos para a melhor visualização em todas
-            as resoluções.
-          </p>
-        </div>
-        <div className={styles.card}>
-          <span>Código semântico</span>
-          <BsCodeSquare style={{ color: 'white', width: '70px', height: '70px' }} />
-          <p>
-            Estratégia e monitoração para seu negócio, fazendo com que seu site apareça cada vez melhor nas buscas.          </p>
-        </div>
-      </div>
-    </div >
+      </div >
+    </IconContext.Provider>
   )
 }
